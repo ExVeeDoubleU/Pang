9b705d98783efb0f3b2a70c5ebb8d5a697763330
robinstrom <robinstrom94@gmail.com>
2017-10-18 13:39:30 +0200

    Laser color

--- src/Base/Laser.java
+++ src/Base/Laser.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Laser {
 
-//    private int nextX;
+    //    private int nextX;
 //    private int nextY;
     private int x;
     private int y;
@@ -20,39 +20,40 @@ public class Laser {
         this.appearance = appearance;
     }
 
-    public void move(Terminal terminal){
+    public void move(Terminal terminal) {
         terminal.moveCursor(x, y);
         terminal.putCharacter(' ');
-        x=x+speed;
+        x = x + speed;
 
     }
 
-    public void draw(Terminal terminal){
-            terminal.moveCursor(x, y);
-            terminal.putCharacter(appearance);
-        }
+    public void draw(Terminal terminal) {
+        terminal.moveCursor(x, y);
+        terminal.applyForegroundColor(Terminal.Color.RED);
+        terminal.putCharacter(appearance);
+        terminal.applyForegroundColor(Terminal.Color.DEFAULT);
+    }
 
-    public void hitsTarget(Player p1, Player p2){
+    public void hitsTarget(Player p1, Player p2) {
 
-        for(Body b : p1.getBody())
-        {
-            if(x==b.getX()&&y==b.getY()){
-                p1.setHp(p1.getHp()-1);
+        for (Body b : p1.getBody()) {
+            if (x == b.getX() && y == b.getY()) {
+                p1.setHp(p1.getHp() - 1);
                 b.setAppearance('D');
             }
         }
-        for(Body b : p2.getBody())
-        {
-            if(x==b.getX()&&y==b.getY()){
-                p2.setHp(p2.getHp()-1);
+        for (Body b : p2.getBody()) {
+            if (x == b.getX() && y == b.getY()) {
+                p2.setHp(p2.getHp() - 1);
                 b.setAppearance('w');
             }
         }
     }
-    public void hitsLaser(List<Laser> lasers, List<Laser> removeList){
-        for (int i = lasers.size() -1; i >= 0 ; i--) {
-            if(!this.equals(lasers.get(i))){
-                if (this.x == lasers.get(i).getX() && this.y == lasers.get(i).getY()){
+
+    public void hitsLaser(List<Laser> lasers, List<Laser> removeList) {
+        for (int i = lasers.size() - 1; i >= 0; i--) {
+            if (!this.equals(lasers.get(i))) {
+                if (this.x == lasers.get(i).getX() && this.y == lasers.get(i).getY()) {
                     removeList.add(this);
                     removeList.add(lasers.get(i));
                 }
